59be7ef5006a0e5b89ea6ec26ddff8c75b276d20
Opeyemi Babatunde <babatundeopeyemiyusuf@gmail.com>
2023-05-10 10:03:35 +0100

    Add files via upload

---
+++ new.py
@@ -0,0 +1,38 @@
+"""
+print ("hello world!")
+if 5 > 2:
+    print("five is greater than two")
+"""
+""""
+a = 3
+print(type(a))
+"""
+"""
+b = 2.5
+print(type(b))
+"""
+"""
+a = True
+print(type(a))
+b = False
+print(type(b))
+"""
+"""
+c = "demola"
+print(type(c))
+"""
+"""
+h = ("adebayo", "30", "zero")
+print(type(h))
+"""
+"""
+s = {"40", "segun", "2.5"}
+print(type(s))
+"""
+
+j = {
+    "car": "pajero",
+    "books": "textbook",
+    "movie": "citadel"
+}
+print(type(j))
